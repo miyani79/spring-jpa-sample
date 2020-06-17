d94497780563a837ca19517172afaaa9b52f4e29
Kim,Heetei <miyani79@gmail.com>
2020-06-17 20:06:49 +0900

    commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+this is jpa sample
---
+++ src/main/java/com/miyani/sample/spring/boot/jpa/jpa/JpaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.miyani.sample.spring.boot.jpa.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSampleApplication.class, args);
+	}
+
+}
